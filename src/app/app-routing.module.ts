9a764ca5e911671167a8964ea6194ab22c604558
Bappi Kabir <kabirbapson@gmail.com>
2023-09-26 01:02:05 +0100

    fix: consuming cadl

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { FormsGroupComponent } from './forms-group/forms-group.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AppComponent },
+  {path:'form', component:FormsGroupComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
